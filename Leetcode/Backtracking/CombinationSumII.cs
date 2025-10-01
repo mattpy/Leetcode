f9a700aa87c5aadce714eb031495cc77a6b70f9f
mattpy <Matt@DESKTOP-4QKF1L7>
2025-09-30 19:26:44 -0700

    Solve combination sum 2

---
+++ Leetcode/Backtracking/CombinationSumII.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode.Backtracking;
+
+class CombinationSumII
+{
+    public static IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        Array.Sort(candidates);
+        List<IList<int>> output = new();
+
+        void Recurse(int index, int sum, List<int> current)
+        {
+            if (sum < 0) return;
+            else if (sum == 0)
+            {
+                output.Add([.. current]);
+                return;
+            }
+
+            for (int i = index; i < candidates.Length; i++)
+            {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+
+                current.Add(candidates[i]);
+                Recurse(i + 1, sum - candidates[i], current);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+
+        Recurse(0, target, new());
+
+        return output;
+    }
+
+    public static void Run()
+    {
+        int[] candidates = [10, 1, 2, 7, 6, 1, 5];
+        int target = 8;
+        var result = CombinationSum2(candidates, target);
+        Console.WriteLine(result);
+    }
+}
