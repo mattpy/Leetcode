47b2fbe244f149461008c2ab99d3e14f89487abe
mattpy <Matt@DESKTOP-4QKF1L7>
2025-10-04 13:05:12 -0700

    Solve leetcode 572

--- Leetcode/Trees/SubsetOfAnotherTree.cs
+++ Leetcode/Trees/SubsetOfAnotherTree.cs
@@ -1,5 +1,41 @@
-﻿namespace Leetcode.Trees;
+﻿using Leetcode.Helpers;
+
+namespace Leetcode.Trees;
 
 public class SubsetOfAnotherTree
 {
+    public static bool IsSubtree(TreeNode? root, TreeNode? subRoot)
+    {
+        if (root is null) return false;
+
+        Stack<TreeNode> stack = new();
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            TreeNode node = stack.Pop();
+            if (node.val == subRoot?.val && CheckSubtree(node, subRoot))
+            {
+                return true;
+            }
+
+            if (node.left is not null) stack.Push(node.left);
+            if (node.right is not null) stack.Push(node.right);
+        }
+
+        return false;
+    }
+
+    private static bool CheckSubtree(TreeNode? treeA, TreeNode? treeB)
+    {
+        if (treeA is null && treeB is null)
+            return true;
+        else if (treeA is null || treeB is null)
+            return false;
+        else if (treeA.val != treeB.val)
+            return false;
+        else
+            return CheckSubtree(treeA.left, treeB.left) &&
+                CheckSubtree(treeA.right, treeB.right);
+    }
 }
