d51d73f831723089bd4fb534a2c18cb14aa41592
mattpy <Matt@DESKTOP-4QKF1L7>
2025-10-03 23:17:53 -0700

    Solve leetcode #110

---
+++ Leetcode/Trees/BalancedBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using Leetcode.Helpers;
+
+namespace Leetcode.Trees;
+
+public class BalancedBinaryTree
+{
+    private bool _isBalanced = true;
+
+    public bool IsBalanced(TreeNode root)
+    {
+        FindHeight(root);
+        return _isBalanced;
+    }
+
+    private int FindHeight(TreeNode? node)
+    {
+        if (node is null) return 0;
+
+        int left = FindHeight(node.left);
+        int right = FindHeight(node.right);
+
+        if (Math.Abs(right - left) > 1)
+        {
+            _isBalanced = false;
+        }
+
+        return Math.Max(left, right) + 1;
+    }
+}
