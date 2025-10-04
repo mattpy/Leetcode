6d1cae11fe759534d374f6b9d88fe65f960a661b
mattpy <Matt@DESKTOP-4QKF1L7>
2025-10-04 12:51:01 -0700

    Solve is same tree

---
+++ Leetcode/Trees/SameTree.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode.Helpers;
+
+namespace Leetcode.Trees;
+
+public class SameTree
+{
+    public static bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p is null && q is null)
+            return true;
+        else if (p is null || q is null)
+            return false;
+        else if (p.val != q.val)
+            return false;
+        else
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
