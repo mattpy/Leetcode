afc50fe6b7b7328a6880a6a390c6a6e234bfb63a
mattpy <Matt@DESKTOP-4QKF1L7>
2025-10-05 13:07:08 -0700

    Solve group anagrams

---
+++ Leetcode/Arrays/GroupAnagramsQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode.Arrays;
+
+public class GroupAnagramsQuestion
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        return strs
+            .GroupBy(x => string.Concat(x.OrderBy(c => c)))
+            .Select(x => x.ToArray())
+            .ToArray();
+    }
+}
