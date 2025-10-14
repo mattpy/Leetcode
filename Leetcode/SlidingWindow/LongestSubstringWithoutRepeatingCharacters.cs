75e9cce71b75b5ec077306372aa47417c38a4668
Matt <casper.angelfire@gmail.com>
2025-10-13 22:05:30 -0700

    solve longest substring without repeating characters

---
+++ Leetcode/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.SlidingWindow;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> seen = new();
+
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            while (seen.Contains(s[right]))
+            {
+                seen.Remove(s[left]);
+                left++;
+            }
+            seen.Add(s[right]);
+
+            max = Math.Max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
