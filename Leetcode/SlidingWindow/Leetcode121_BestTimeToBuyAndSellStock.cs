0c307447d0d44c61daad54eb5ddd38de63d71fdf
mattpy <Matt@DESKTOP-4QKF1L7>
2025-11-03 22:43:20 -0800

    Solve best time to buy and sell stock

---
+++ Leetcode/SlidingWindow/Leetcode121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.SlidingWindow;
+
+class Leetcode121_BestTimeToBuyAndSellStock
+{
+    public static int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+        int minPrice = int.MaxValue;
+
+        foreach (int price in prices)
+        {
+            minPrice = Math.Min(minPrice, price);
+            maxProfit = Math.Max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+
+    public static void Run()
+    {
+        int result = MaxProfit([7, 1, 5, 3, 6, 4]);
+        Console.WriteLine(result);
+    }
+}
