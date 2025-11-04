90aaf1eae973f8012c791a4c4572819fe2f57c61
mattpy <Matt@DESKTOP-4QKF1L7>
2025-11-03 22:57:32 -0800

    refactor and solve longest repeating character replacement

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,12 +1,11 @@
 ﻿using Leetcode.Tries;
 
-namespace Leetcode
+namespace Leetcode;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            new ImplementTriePrefixTree().Run();
-        }
+        //
     }
 }
