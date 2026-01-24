e29b968a69026c07488d5d4642e950a1a922855c
mattpy <Matt@DESKTOP-4QKF1L7>
2026-01-23 20:34:24 -0800

    Solve Leetcode 271

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace Leetcode;
 
-using Leetcode.Arrays;
+using Leetcode.Strings;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Leetcode_217__Contains_Duplicate.Run();
+        Leetcode_271__Encode_And_Decode_Strings.Encode(["Hello", "World"]);
     }
 }
---
+++ Leetcode/Strings/Leetcode_271__Encode_And_Decode_Strings.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Leetcode.Strings;
+
+public class Leetcode_271__Encode_And_Decode_Strings
+{
+    private const string _delimiter = "!!@@!!";
+
+    public static string Encode(IList<string> strs)
+    {
+        StringBuilder sb = new();
+
+        for (int i = 0; i < strs.Count; i++)
+        {
+            sb.Append(strs[i]);
+            if (i != strs.Count - 1)
+            {
+                sb.Append(_delimiter);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static IList<string> Decode(string s)
+    {
+        return s.Split(_delimiter);
+    }
+}
