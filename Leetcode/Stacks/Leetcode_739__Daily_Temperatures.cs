a89e8fadaf3c2948f40fea26c00abc21fbed195c
mattpy <Matt@DESKTOP-4QKF1L7>
2026-01-25 00:04:34 -0800

    Solve Leetcode 739 Daily Temperatures

---
+++ Leetcode/Stacks/Leetcode_739__Daily_Temperatures.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode.Stacks;
+
+public class Leetcode_739__Daily_Temperatures
+{
+    public static int[] DailyTemperatures(int[] temperatures)
+    {
+        int n = temperatures.Length;
+
+        Stack<(int index, int temp)> stack = new(n);
+        int[] output = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = temperatures[i];
+
+            while (stack.Count > 0 && temp > stack.Peek().temp)
+            {
+                (int prevIndex, _) = stack.Pop();
+                output[prevIndex] = i - prevIndex;
+            }
+
+            stack.Push((i, temp));
+        }
+
+        return output;
+    }
+
+    public static void Run()
+    {
+        int[] input = [73, 74, 75, 71, 69, 72, 76, 73];
+        var result = DailyTemperatures(input);
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
