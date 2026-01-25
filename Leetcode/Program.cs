ecff8e7307a082a38b675d2994a3be993c19aad9
mattpy <Matt@DESKTOP-4QKF1L7>
2026-01-24 18:34:10 -0800

    Solve valid parentheses

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -6,6 +6,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Leetcode_271__Encode_And_Decode_Strings.Encode(["Hello", "World"]);
+
     }
 }
---
+++ Leetcode/Stacks/Leetcode_20__Valid_Parentheses.cs
@@ -0,0 +1,32 @@
+﻿namespace Leetcode.Stacks;
+
+public class Leetcode_20__Valid_Parentheses
+{
+    public bool IsValid(string s)
+    {
+        Dictionary<char, char> matches = new()
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' },
+        };
+
+        Stack<char> seen = new();
+
+        foreach (char letter in s)
+        {
+            if (!matches.ContainsKey(letter))
+            {
+                seen.Push(letter);
+                continue;
+            }
+
+            if (seen.Count == 0 || matches[letter] != seen.Pop())
+            {
+                return false;
+            }
+        }
+
+        return seen.Count == 0;
+    }
+}
