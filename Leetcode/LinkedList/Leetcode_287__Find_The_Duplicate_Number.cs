6b66a58f40d882893ba76115a6a134e4f7085ae3
mattpy <Matt@DESKTOP-4QKF1L7>
2026-02-07 23:35:58 -0800

    Solve leetcode 287

---
+++ Leetcode/LinkedList/Leetcode_287__Find_The_Duplicate_Number.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.LinkedList;
+
+public class Leetcode_287__Find_The_Duplicate_Number
+{
+    public static int FindDuplicate(int[] nums)
+    {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return fast;
+    }
+}
