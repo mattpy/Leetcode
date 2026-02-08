6c5b9b65718188f5ce0590f01857c46b007634fa
mattpy <Matt@DESKTOP-4QKF1L7>
2026-02-07 23:25:29 -0800

    Solve linked list cycle

---
+++ Leetcode/DynamicProgramming/Leetcode_45__Jump_Game_II.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.DynamicProgramming;
+
+public class Leetcode_45__Jump_Game_II
+{
+    public static int Jump(int[] nums)
+    {
+        int answer = 0;
+        int currentEnd = 0;
+        int currentFarthest = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            currentFarthest = Math.Max(currentFarthest, i + nums[i]);
+
+            if (i == currentEnd)
+            {
+                answer++;
+                currentEnd = currentFarthest;
+            }
+        }
+
+        return answer;
+    }
+}
