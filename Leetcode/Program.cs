9698c9bf0c9140370b11da36d9299492a7aec3d1
mattpy <Matt@DESKTOP-4QKF1L7>
2026-02-18 19:09:22 -0800

    Solve leetcode 34

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Leetcode;
+﻿using Leetcode.BinarySearch;
+
+namespace Leetcode;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-
+        int[] nums = { 1, 1, 1, 2, 2, 2, 2, 2, 3, 4, 5 };
+        int target = 1;
+        Leetcode_34__Find_First_And_Last_Position.SearchRange(nums, target);
     }
 }
