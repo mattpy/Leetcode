63920c1de308f35e5b4ced25c87e2e16266f5e27
mattpy <Matt@DESKTOP-4QKF1L7>
2026-02-22 17:10:25 -0800

    Solve trie leetcode problem

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,28 +1,6 @@
-﻿using Leetcode.Helpers;
-using Leetcode.LinkedList;
-
-namespace Leetcode;
+﻿namespace Leetcode;
 
 internal class Program
 {
-    static void Main(string[] args)
-    {
-        ListNode l1 = new(1);
-        l1.next = new(3);
-        l1.next.next = new(7);
-        l1.next.next.next = new(11);
 
-        ListNode l2 = new(2);
-        l2.next = new(4);
-        l2.next.next = new(4);
-        l2.next.next.next = new(10);
-
-        var result = Leetcode_21__Merge_Two_Sorted_Lists.MergeTwoLists(l1, l2);
-
-        while (result is not null)
-        {
-            Console.WriteLine(result.val);
-            result = result.next;
-        }
-    }
 }
