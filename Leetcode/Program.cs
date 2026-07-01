bade989df8eb77012d9b3b096cf98929defc000f
mattpy <46332744+mattpy@users.noreply.github.com>
2026-06-30 23:26:03 -0700

    solve problem

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,10 +1,10 @@
-﻿using Leetcode.Arrays;
-
-namespace Leetcode;
+﻿namespace Leetcode;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var result = SlidingWindow.Leetcode3_LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("abcabcbb");
+        Console.WriteLine(result);
     }
 }
---
+++ Leetcode/SlidingWindow/Leetcode3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode.SlidingWindow;
+
+public class Leetcode3_LongestSubstringWithoutRepeatingCharacters
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> seen = new();
+        int maxLength = 0;
+        int currentLength = 0;
+
+        int left = 0;
+        for (int right = 0; right < s.Length; right++)
+        {
+            char rightChar = s[right];
+
+            while (seen.Contains(rightChar))
+            {
+                seen.Remove(s[left]);
+                left++;
+            }
+
+            seen.Add(s[right]);
+
+            currentLength = right - left + 1;
+            maxLength = Math.Max(maxLength, currentLength);
+        }
+
+        return maxLength;
+    }
+}
