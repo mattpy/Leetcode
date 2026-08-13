c4efc8f14aacb069d91683bc7aa2ff35a976ee37
mattpy <46332744+mattpy@users.noreply.github.com>
2026-08-12 20:39:41 -0700

    Solve rotting oranges

---
+++ Leetcode/Graphs/Leetcode_994__Rotting_Oranges.cs
@@ -0,0 +1,56 @@
+﻿namespace Leetcode.Graphs;
+
+public class Leetcode_994__Rotting_Oranges
+{
+    public static int OrangesRotting(int[][] grid)
+    {
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        int numberFresh = 0;
+
+        Queue<(int row, int col)> queue = new();
+
+        for (int row = 0; row < grid.Length; row++)
+        {
+            for (int col = 0; col < grid[0].Length; col++)
+            {
+                if (grid[row][col] == 1) numberFresh++;
+
+                if (grid[row][col] == 2)
+                {
+                    queue.Enqueue((row, col));
+                }
+            }
+        }
+
+        if (numberFresh == 0) return 0;
+
+        ReadOnlySpan<(int dx, int dy)> offsets = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        int secondsPassed = 0;
+
+        while (queue.Count > 0 && numberFresh > 0)
+        {
+            int size = queue.Count;
+            for (int i = 0; i < size; i++)
+            {
+                var (row, col) = queue.Dequeue();
+
+                foreach (var (dx, dy) in offsets)
+                {
+                    int newRow = row + dx;
+                    int newCol = col + dy;
+
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == 1)
+                    {
+                        numberFresh--;
+                        grid[newRow][newCol] = 2;
+                        queue.Enqueue((newRow, newCol));
+                    }
+                }
+            }
+            secondsPassed++;
+        }
+
+        return numberFresh == 0 ? secondsPassed : -1;
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,10 +1,15 @@
-﻿using Leetcode.Arrays;
-
-namespace Leetcode;
+﻿namespace Leetcode;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        int result1 = Graphs.Leetcode_994__Rotting_Oranges.OrangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]);
+        int result2 = Graphs.Leetcode_994__Rotting_Oranges.OrangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]);
+        int result3 = Graphs.Leetcode_994__Rotting_Oranges.OrangesRotting([[0, 2]]);
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);
     }
 }
